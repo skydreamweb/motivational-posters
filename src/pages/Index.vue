6ca60f619f2ea8306d4073289d806d70d1f9fd2d
skydreamweb <info@skydream.info>
2020-10-26 13:22:55 +0100

    Q-file and Q-img fileds

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,30 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+   
+      <q-file outlined v-model="model">
+        <template v-slot:prepend>
+          <q-icon name="attach_file" />
+        </template>
+      </q-file>
+
+      <q-img
+      v-for="img in images"
+      :key="img.id"
+      :src="img.src"
+      @click="selectImageHandler(img)"
+      >
+      </q-img>
+
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data(){
+    return {
+      model: null
+    }
+  }
 }
 </script>
