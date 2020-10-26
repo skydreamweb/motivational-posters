5eb81ede434db59a367cee07b980b7921944c4d9
skydreamweb <info@skydream.info>
2020-10-26 13:57:03 +0100

    Methods
    Add methods

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,30 +1,62 @@
 <template>
   <q-page class="flex flex-center">
-   
-      <q-file outlined v-model="model">
-        <template v-slot:prepend>
-          <q-icon name="attach_file" />
-        </template>
-      </q-file>
-
-      <q-img
+  
+    <input type="file" @change="fileUpload">
+    <img
       v-for="img in images"
       :key="img.id"
       :src="img.src"
       @click="selectImageHandler(img)"
-      >
-      </q-img>
-
+    >
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex',
-  data(){
+  name: "PageIndex",
+  data() {
     return {
-      model: null
+      model: null,
+      images: []
+    };
+  },
+methods: {
+    fileUpload(event) {
+      const reader = new FileReader()
+      reader.addEventListener('load', () => {
+        // create unique name
+        let id = 0
+        let images = JSON.parse(localStorage.getItem('images'))
+        if (images && images.length > 0) {
+          id = Number(images[images.length - 1].id) + 1
+        }
+        let name = 'img-' + id
+
+        // push image in array
+        this.images.push({ id, name, src: reader.result })
+
+        // put array in localstorage
+        localStorage.setItem('images', JSON.stringify(this.images))
+      })
+      reader.readAsDataURL(event.target.files[0])
+    },
+    // get info about image on click
+    selectImageHandler(img) {
+      console.log(img)
+    },
+  },
+  created() {
+    // load data from localstorage after reload component
+    let images = []
+    images = JSON.parse(localStorage.getItem('images'))
+      ? JSON.parse(localStorage.getItem('images'))
+      : []
+
+    if (images.length > 0) {
+      images.forEach(image => {
+        this.images.push(image)
+      })
     }
-  }
-}
+  },
+};
 </script>
