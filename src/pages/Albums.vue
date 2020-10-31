2b8805e53cabd796d6c9be374a5946d5c3868df8
skydreamweb <info@skydream.info>
2020-10-31 23:12:05 +0100

    Update Albums.vue
    Create q-list

--- src/pages/Albums.vue
+++ src/pages/Albums.vue
@@ -1,25 +1,68 @@
 <template>
   <div>
     <q-list class="flex" bordered padding>
-      <q-item v-ripple v-if="posters.length > 0">
+      <q-item v-ripple v-if="albums.length > 0">
         <div class="container row">
           <div
-            v-for="poster in posters"
-            :key="poster.posterId"
-            class="images col-3 poster-list"
+            v-for="album in albums"
+            :key="album.albumId"
+            class="albums col-3 albums-list"
           >
             <q-checkbox
               indeterminate-value="false"
               v-model="checkedPosterIds"
-              :val="poster.posterId"
+              :val="album.albumId"
             />
+            
             <div class="inner">
-              <img :src="poster.imgSrc" />
-              <div class="lable">
-                <label>{{ poster.title }}</label>
-                <label>{{ poster.subtitle }}</label>
+              <div>
+                <h3>Album name: {{ album.name }}</h3>
               </div>
             </div>
+
+            <!-- Posters -->
+            <h3>Posters list</h3>
+            <q-list class="flex" bordered padding>
+              <q-item v-ripple v-if="posters.length > 0">
+                <div class="container row">
+                  <div
+                    v-for="poster in posters"
+                    :key="poster.posterId"
+                    class="images col-3 poster-list"
+                  >
+                    <q-checkbox
+                      indeterminate-value="false"
+                      v-model="checkedPosterIds"
+                      :val="poster.posterId"
+                      @click.native="checkHandler()"
+                    />
+                    <div class="inner">
+                      <img :src="poster.imgSrc" />
+                      <div class="lable">
+                        <label>{{ poster.title }}</label>
+                        <label>{{ poster.subtitle }}</label>
+                      </div>
+                    </div>
+                    <div class="text">
+                      <div class="button">
+                        <button
+                          @click="deletePosterHandler(poster.posterId)"
+                          class=""
+                          separator
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </q-item>
+              <q-item v-else>No posters found</q-item>
+
+              <q-separator spaced />
+            </q-list>
+            <!-- End posters -->
+
             <div class="text">
               <div class="button">
                 <button class="" separator>
@@ -42,11 +85,39 @@ export default {
   name: "Albums",
   data() {
     return {
+      albums: [],
       posters: [],
       checkedPosterIds: []
     };
   },
+  methods: {
+    checkHandler() {
+      console.log(this.checkedPosterIds);
+      let selectedImages = this.posters.filter(poster =>
+        this.checkedPosterIds.includes(poster.posterId)
+      );
+      // send all checked posters
+      this.$emit("posterChecked", selectedImages);
+    },
+    deletePosterHandler(posterId) {
+      // remove poster
+      this.posters = this.posters.filter(
+        poster => poster.posterId !== posterId
+      );
+      // save change to local storage
+      localStorage.removeItem("posters");
+      localStorage.setItem("posters", JSON.stringify(this.posters));
+    }
+  },
   mounted() {
+    let albums = JSON.parse(localStorage.getItem("albums"))
+      ? JSON.parse(localStorage.getItem("albums"))
+      : [];
+    if (albums.length > 0) {
+      albums.forEach(album => {
+        this.albums = albums;
+      });
+    }
     let posters = JSON.parse(localStorage.getItem("posters"))
       ? JSON.parse(localStorage.getItem("posters"))
       : [];
@@ -60,6 +131,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.albums {
+  width: 100%;
+  height: auto;
+  border: 1px solid black;
+}
 .poster-list {
   border: 1px solid black;
   padding: 5px;
